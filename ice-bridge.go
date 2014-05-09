a7e168c70874c7a2fd6a0445bff50b368bbcfc9a
Jesse Taber <jtaber@articulate.com>
2014-05-09 11:58:24 -0400

    first pass at iterating files and downloadng them

--- ice-bridge.go
+++ ice-bridge.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	_ "strconv"
 )
 
 func main() {
- fmt.Println("Hello from the ice-box")
+	fmt.Println("Hello from the ice-box")
+
+	//TODO: these should maybe be command-line args
+	var localPath = `C:\dev\dropbox_archive`
+	var dropboxPath = "images/wallpapers"
+	var err = archiveFolder(dropboxPath, localPath)
+
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+		return
+	}
+
 }
