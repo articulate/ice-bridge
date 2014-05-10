fd928f6fd677ff149f5ad231d80873826d55aa6a
Alex Arnell <alex.arnell@gmail.com>
2014-05-09 19:18:19 -0700

    Adding concurrent download capabilities.
    I mostly followed along with this paper and modelled the walking and
    processing after the bounded.go example.
    
    http://blog.golang.org/pipelines

--- dropbox.go
+++ dropbox.go
@@ -2,38 +2,102 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"errors"
 	"github.com/stacktic/dropbox"
 	"path/filepath"
 )
 
 func archiveFolder(config *ConfigFile) error {
+	const numDownloaders = 5
 
-	var fileEntries []dropbox.Entry
-	var err error
+	done := make(chan struct{})
+	defer close(done)
 
-	fileEntries, err = getFiles(config)
-	if err != nil {
+	files, errc := walkFiles(done, config)
+
+	// spawn up a bunch of concurrent downloaders
+	var wg sync.WaitGroup
+	wg.Add(numDownloaders)
+	c := make(chan download)
+	for i := 0; i < numDownloaders; i++ {
+		go func() {
+			downloader(config, done, files, c)
+			wg.Done()
+		}()
+	}
+
+	// wait for all the downloaders to finish
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
+	// examine all the download results looking for an error
+	for r := range c {
+		if r.err != nil {
+			return r.err
+		}
+	}
+
+	// finally check to see if the original file walk failed
+	if err := <-errc; err != nil {
 		return err
 	}
 
-	for _, file := range fileEntries {
-		var fileName string
+	return nil
+}
 
-		fileName = filepath.Base(file.Path)
-		if file.IsDir {
-			fmt.Printf("%v is a directory; skipping\n", file.Path)
-			continue
+type download struct {
+	path string
+	revision string
+	err error
+}
+
+func walkFiles(done <-chan struct{}, config *ConfigFile) (<-chan dropbox.Entry, <-chan error) {
+	files := make(chan dropbox.Entry)
+	errc := make(chan error, 1)
+
+	go func() {
+		defer close(files)
+
+		var fileEntries []dropbox.Entry
+		var err error
+
+		fileEntries, err = getFiles(config)
+		if err != nil {
+			errc <- err
 		}
 
-		fmt.Printf("downloading file: %v - rev = %v\n", fileName, file.Revision)
+		for _, file := range fileEntries {
+			if file.IsDir {
+				fmt.Printf("%v is a directory; skipping\n", file.Path)
+				continue
+			}
 
-		err = downloadFile(config, file.Path, filepath.Join(config.LocalPath, fileName))
-		if err != nil {
-			fmt.Println("error downloading: " + file.Path)
-			continue
+			select {
+			case files <- file:
+			case <-done:
+				errc <- errors.New("walk cancelled")
+			}
+		}
+		errc <- nil
+	}()
+
+	return files, errc
+}
+
+func downloader(config *ConfigFile, done <-chan struct{}, files <-chan dropbox.Entry, c chan<- download) {
+	for file := range files {
+		fileName := filepath.Base(file.Path)
+		fmt.Printf("downloading file: %v - rev = %v\n", file.Path, file.Revision)
+		err := downloadFile(config, file.Path, filepath.Join(config.LocalPath, fileName))
+		select {
+		case c <- download{file.Path, file.Revision, err}:
+		case <-done:
+			return
 		}
 	}
-	return err
 }
 
 func getFiles(config *ConfigFile) ([]dropbox.Entry, error) {
